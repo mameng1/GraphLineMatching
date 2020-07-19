24c90a3cbe2c2eed17d152365487b19d3d021ae5
mameng1 <2908071445@qq.com>
2020-07-19 22:42:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -263,7 +263,7 @@ def eval_model(l_img,r_img,l_boxes,r_boxes,l_pts,r_pts,model,model_path):
     with torch.set_grad_enabled(False):
         score_thresh=0.3
         s_pred, _,_,_,_,_,indeces1, indeces2, newn1_gt, newn2_gt = \
-            model(l_img, r_img, l_boxes, r_boxes, l_pts, r_pts,perm_mat=None,
+            model(l_img, r_img, l_boxes, r_boxes, l_pts, r_pts,perm_mat=None,train_stage=False,
                   score_thresh=score_thresh)
         s_pred_perm = lap_solver(s_pred, newn1_gt, newn2_gt, indeces1, indeces2, l_pts, r_pts)
         #s_pred_perm = lap_solver(s_pred, None, l_pts, r_pts)
